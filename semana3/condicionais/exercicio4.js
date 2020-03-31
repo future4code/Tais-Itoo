080188c7b076e1466f8efb25e7449c83dd3a5ec8
Tais Itoo <tais2001@gmail.com>
2020-03-31 19:00:26 -0300

    condicionais Aula 10

---
+++ semana3/condicionais/exercicio4.js
@@ -0,0 +1,12 @@
+// a. Se os 2 números forem iguais não há resposta.
+// b. 
+
+
+
+const n1 = (prompt("Digite o primeiro número:"));
+const n2 = (prompt("Digite o segundo número:"));
+
+if (n1 > n2) {
+    console.log("A ordem descrescente é ", n1, " e ", n2)
+
+}
