f81e16b1ee009a8117d175c14d0ee1be67c71e3f
Tais Itoo <tais2001@gmail.com>
2020-04-09 17:13:29 -0300

    semana4-aula16

---
+++ semana4/funcoes-de-array/exercicios.js
@@ -0,0 +1,12 @@
+//1.Cite 3 maneiras de se percorrer/iterar um array, que você conheceu antes das funções de array. 
+//Faça um programa para exemplificar.
+//2.Considere um array com o seguinte formato:
+//a.Faça uma função que retorne um novo array só com os adultos (pessoas com idade igual ou superior a 20)
+//b.Faça uma função que retorne um novo array só com as crianças/adolescentes (pessoas com idade inferior a 20)
+
+[
+    { nome: "Pedro", idade: 20 },
+    { nome: "João", idade: 10 },
+    { nome: "Paula", idade: 12 },
+    { nome: "Artur", idade: 89 }
+]
