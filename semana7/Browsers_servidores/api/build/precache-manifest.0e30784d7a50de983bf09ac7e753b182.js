978c63a40438697d9049f1dc923a62b85dd863a0
Tais Itoo <tais2001@gmail.com>
2020-05-15 17:40:47 -0300

    projeto astromatch para mobile incompleto

---
+++ semana7/Browsers_servidores/api/build/precache-manifest.0e30784d7a50de983bf09ac7e753b182.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f57e4bd5affac324a277905cd07a4f70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2905beec1ca568d807e",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "67527b8c2ec1b311b201",
+    "url": "/static/js/2.389af32f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.389af32f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2905beec1ca568d807e",
+    "url": "/static/js/main.7af1f9cd.chunk.js"
+  },
+  {
+    "revision": "464b2beb01c60ad56ef2",
+    "url": "/static/js/runtime-main.d4bbcad1.js"
+  }
+]);
