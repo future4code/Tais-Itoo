420236fe36b9be0debd5eb21c73062e750da5a4b
Tais Itoo <tais2001@gmail.com>
2020-05-18 18:04:47 -0300

    aula33

---
+++ semana8/astromatch/astromatch/src/styles.js
@@ -0,0 +1,34 @@
+import styled from "styled-components"
+
+
+export const Imglogo = styled.img` 
+width:40vw;
+`   
+
+export const PostContainer = styled.div`
+width: 100vw;
+height: 100vh;
+display:flex;
+flex-direction:column;
+` 
+
+export const PostHeader = styled.div`
+height: 10vh;
+display: flex;
+align-items: center;
+padding-left: 10vw;
+` 
+
+export const PostPhoto = styled.img`
+width: 40vw;
+align-self:center;
+justify-self:center;
+`   
+
+export const PostPerfil = styled.div`
+display: flex;
+align-items: center;
+padding: 1vw;
+justify-content: center;
+flex-direction:column;
+`   
